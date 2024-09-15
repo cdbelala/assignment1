e1bc038a13459303753540e416b87921e05ce3ae
cdbelala <149855573+cdbelala@users.noreply.github.com>
2024-09-15 17:45:44 -0500

    completed phonebook.rb

--- src/phonebook.rb
+++ src/phonebook.rb
@@ -1,20 +1,36 @@
 class PhoneBook
     def initialize
+        @phonebook = {}
     end
 
     def add(name, number, is_listed)
-        raise Exception, "Not implemented"
+        @phonebook[name] = {phoneNum: number, listed: is_listed}
     end
 
     def lookup(name)
-        raise Exception, "Not implemented"
+        lookup = @phonebook[name]
+        return nil unless lookup
+        "Name: #{lookup[:name]} | Phone Number: #{lookup[:phoneNum]} | Listed: #{lookup[:listed]}"
+        return lookup.phoneNum
     end
 
     def lookupByNum(number)
-        raise Exception, "Not implemented"
+        @phonebook.each do |name, lookup|
+            return name if lookup[:phoneNum] == number
+        end
+        return nil
     end
 
     def namesByAc(areacode)
-        raise Exception, "Not implemented"
+        matches = []
+        @phonebook.each do |name, lookup|
+            if lookup[:phoneNum] && lookup[:phoneNum].start_with?(areacode)
+                matches << name
+            end
+        end
+        return matches
     end
 end
+  @phonebook = PhoneBook.new
+
+@phonebook.lookup("John")
